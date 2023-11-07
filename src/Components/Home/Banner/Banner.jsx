079acefce6a3f2bccb4840cde6df8b08010f8209
MD MASUM <mdmasumdu45@gmail.com>
2023-11-07 21:49:48 +0600

    added another slider to banner

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 // import Swiper core and required modules
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Link } from "react-router-dom";
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -10,6 +11,7 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 const Banner = () => {
+  
     return (
         <div>
 
@@ -24,12 +26,14 @@ const Banner = () => {
      
     >
       <SwiperSlide>
-            <div className='flex'>
+            <div className='md:flex'>
                 <div className='flex-1 flex flex-col justify-center items-center'>
 
                     <p className='text-4xl font-bold'>Share Your <span className='text-orange-500'>Food</span></p>
                     <p className='text-4xl font-bold'>Take Yours</p>
-                    <button className='btn mt-5 bg-orange-300'>Start Sharing</button>
+                    <Link to={"/addfood"}><button className='btn mt-5 bg-orange-300'>Start Sharing</button> </Link>
+                    
+                    
 
                   
                 </div>
@@ -40,7 +44,7 @@ const Banner = () => {
             </div></SwiperSlide>
 
       <SwiperSlide>
-            <div className='flex'>
+            <div className='md:flex'>
             <div className='flex-1'>
                     <img src={'https://i.ibb.co/n0nGbdc/6349642-removebg-preview.png'} alt="" />
 
@@ -49,9 +53,26 @@ const Banner = () => {
 
                     <p className='text-4xl font-bold'>Do not let your <span className='text-orange-500'>Food</span> <br /></p>
                     <p className='text-4xl font-bold text-red-500'>to be wasted</p>
-                    <button className='btn mt-5 bg-orange-300'>Start Sharing</button>
+                    <Link to={"/addfood"}><button className='btn mt-5 bg-orange-300'>Start Sharing</button> </Link>
 
                 </div>
+               
+            </div></SwiperSlide>
+      <SwiperSlide>
+            <div className='md:flex'>
+
+            <div className='flex-1 flex flex-col justify-center items-center'>
+
+<p className='text-4xl font-bold'>Make people happy  </p>
+<p className='text-4xl font-bold text-red-500'><span className='text-orange-500'>With your Food</span></p>
+<Link to={"/addfood"}><button className='btn mt-5 bg-orange-300'>Start Sharing</button> </Link>
+
+</div>
+            <div className='flex-1'>
+                    <img src={'https://i.ibb.co/Ns7mqwW/6349457-removebg-preview.png'} alt="" />
+
+                </div>
+                
                
             </div></SwiperSlide>
     
